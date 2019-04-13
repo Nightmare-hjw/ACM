8f302a0067c73a0cf23698fb114d8a9d7ab4eeed
Nightmare-hjw <ishuangjw@foxmailcom>
2019-04-13 23:39:42 +0800

    leetCode

---
+++ LeetCode/src/acm/leetcode/Test.java
@@ -0,0 +1,8 @@
+package acm.leetcode;
+
+public class Test {
+    public static void main(String[] args) {
+        Palindrome_Easy test = new Palindrome_Easy();
+        System.out.println(test.isPalindrome(10));
+    }
+}
